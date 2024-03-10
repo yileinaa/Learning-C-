d204b421b2d7f08d025381eec2e16e848984c145
Tang <1463343010@qq.com>
2024-03-10 23:33:18 +0800

    all

--- 2024.3.9/2024.3.9/test.cpp
+++ 2024.3.9/2024.3.9/test.cpp
@@ -1,1 +1,56 @@
 #include <iostream>
+class Pet
+{
+public:
+	Pet(std::string theName);
+	~Pet();
+	static int getCount();
+protected:
+	std::string name;
+private:
+	static int count;
+};
+class Cat :public Pet
+{
+public:
+	Cat(std::string theName);
+};
+class Mouse :public Pet
+{
+public:
+	Mouse(std::string theName);
+};
+Pet::Pet(std::string theName)
+{
+	name = theName;
+	count++;
+	std::cout << "一只宠物出生了，名字叫做" << name << "\n";
+}
+Pet::~Pet()
+{
+	count--;
+	std::cout << name << "挂掉了\n";
+}
+int Pet::getCount()
+{
+	return count;
+}
+Mouse::Mouse(std::string theName) :Pet(theName)
+{
+}
+Cat::Cat(std::string theName) :Pet(theName)
+{
+}
+int main()
+{
+	Cat cat("Tom");
+	Mouse mouse("Jerry");
+	std::cout << "\n已经诞生了" << Pet::getCount() << "只宠物\n\n";
+	{
+		Cat cat2("Tom");
+		Mouse mouse2("Jerry");
+		std::cout << "\n现在呢，已经诞生了" << Pet::getCount() << "只宠物\n\n";
+	}
+	std::cout << "\n现在呢，还剩下" << Pet::getCount() << "只宠物\n\n";
+	return 0;
+}
